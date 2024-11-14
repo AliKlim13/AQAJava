72d318288849a26721a5ff2839786ffcab819af3
админ <alisa.love95@mail.ru>
2024-11-14 14:54:49 +0300

    task

---
+++ src/Lesson_6/Main.java
@@ -0,0 +1,46 @@
+package Lesson_6;
+
+import static Lesson_6.ArraySum.sumArray;
+
+public class Main {
+    public static void main(String[] args) {
+        // Пример двумерного массива
+        String[][] validArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        String[][] invalidSizeArray = {
+                {"1", "2"},
+                {"3", "4"}
+        };
+
+        String[][] invalidDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "six", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int result = sumArray(validArray);
+            System.out.println("Сумма элементов: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            sumArray(invalidSizeArray);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            sumArray(invalidDataArray);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
