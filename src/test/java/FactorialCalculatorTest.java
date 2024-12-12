d46fbeb1b02159d268a055edffccac7604179248
Alisa Klochkova <alisa.love95@mail.ru>
2024-12-12 16:11:46 +0300

    JUnit5

--- src/test/java/FactorialCalculatorTest.java
+++ src/test/java/FactorialCalculatorTest.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import static org.junit.jupiter.api.Assertions.*;
+import org.example.FactorialCalculator;
+import org.junit.jupiter.api.Test;
+
 
 public class FactorialCalculatorTest {
+    @Test
+    public void testFactorialOfZero(){
+        assertEquals(1, FactorialCalculator.factorial(0));
+    }
+
+    @Test
+    public void testFactorialOfPositiveNum(){
+        assertEquals(120, FactorialCalculator.factorial(5));
+        assertEquals(720, FactorialCalculator.factorial(6));
+    }
+
+    @Test
+    public void testFactorialOfNegativeNun(){
+        Exception exception =
+                assertThrows(IllegalArgumentException.class
+                        ,() ->{
+                    FactorialCalculator.factorial(-1);
+        });
+        assertEquals("Факториал не определен для отрицательных чисел", exception.getMessage());
+    }
+
 }
